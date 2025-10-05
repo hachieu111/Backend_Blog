bb1239807edc721b1fb65180cd6d3f413f484505
Hoàng Hạc Hiếu <hachieu375@gmail.com>
2025-10-05 11:53:36 +0700

    deleted Fullname in ApplicationUser

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,7 +3,5 @@ using Microsoft.AspNetCore.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    
-    public string FullName { get; set; }
    public ICollection<Post> Posts { get; set; } = new List<Post>();
 }
